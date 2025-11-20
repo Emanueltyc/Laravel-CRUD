c3f8261a6879d0dd93a4e7c0ce7ff9a11bacb3fc
Emanuel <jose.eosousa@gmail.com>
2025-11-20 12:02:20 -0300

    feat: add auth middleware to users and roles routes

--- routes/api.php
+++ routes/api.php
@@ -10,5 +10,5 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware(['auth:api']);
 Route::post('/refresh', [AuthController::class, 'refresh'])->middleware(['auth:api']);
-Route::apiResource('/users', UserController::class);
-Route::apiResource('/roles', RoleController::class);
+Route::apiResource('/users', UserController::class)->middleware(['auth:api']);
+Route::apiResource('/roles', RoleController::class)->middleware(['auth:api']);
